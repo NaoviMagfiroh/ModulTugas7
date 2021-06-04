589aa00bdbeae1f5b7e5d5931d28426e5da6c585
NAOVII <naovimagfiroh@gmail.com>
2021-06-04 12:23:20 +0700

    pesan

---
+++ src/Queue/AntrianApp.java
@@ -0,0 +1,27 @@
+package Queue;
+
+public class AntrianApp {
+    public static void main(String[] args) {
+        Antrian antrian = new Antrian(5);
+        antrian.masuk(40);
+        antrian.lihat();
+        System.out.println("");
+        antrian.masuk(33);
+        antrian.lihat();
+        System.out.println("");
+        System.out.println("nilai yang paling depan = " + antrian.peekDepan());
+        antrian.masuk(60);
+        antrian.lihat();
+        System.out.println("");
+        System.out.println("yang diambil dari antrian = " + antrian.keluar());
+        antrian.lihat();
+        System.out.println("");
+        System.out.println("yang diambil dari antrian = " + antrian.keluar());
+        antrian.lihat();
+        System.out.println("");
+        antrian.masuk(54);
+        antrian.lihat();
+        System.out.println("");
+        System.out.println("nilai yang paling depan = " + antrian.peekDepan());
+    }
+}
